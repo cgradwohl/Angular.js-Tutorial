1c3a108d793a2c423d1c3a1825b6dee505dafa8d
Chris Gradwohl <christophergradwohl@gmail.com>
2016-09-14 15:43:28 -0700

    tutorial progress and edits

--- js/exam1.js
+++ js/exam1.js
@@ -1,8 +1,10 @@
 //The angular.module is a global place for creating, registering and retrieving Angular modules.
 //All modules (angular core or 3rd party) that should be available to an application must be
 // registered using this mechanism.
-//A module is a collection of services, directives, controllers, filters, and configuration
-//information. angular.module is used to configure the $injector.
+
+
+// A module is a collection of services, directives, controllers, filters, and configuration
+// information. angular.module is used to configure the $injector.
 var app1 = angular.module('app1', []);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -15,17 +17,22 @@ var app1 = angular.module('app1', []);
 // available to 'ctrl1'
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 app1.controller('ctrl1', function($scope) {
-  //~~~~~~~~~~~~
-  //properties
-  //~~~~~~~~~~~~
-  $scope.first = 1;
-  $scope.second = 1;
 
-  //~~~~~~~~~~~~
-  //methods
-  //~~~~~~~~~~~~
-  $scope.updateValue = function() {
-    $scope.calculation = $scope.first + ' + ' + $scope.second +
-      ' = ' + (+$scope.first + +$scope.second);
-  };
+
+    //~~~~~~~~~~~~
+    //fields
+    //~~~~~~~~~~~~
+    $scope.first = 1;
+    $scope.second = 1;
+
+
+    //~~~~~~~~~~~~
+    //methods
+    //~~~~~~~~~~~~
+    $scope.updateValue = function() {
+        $scope.calculation = $scope.first + ' + ' + $scope.second +
+        ' = ' + (+$scope.first + +$scope.second);
+    };
+
+
 });
